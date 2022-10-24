8a0241571b9fc9167269a94b39aaa4c203515a2a
nextdorf <nextdorf@users.noreply.github.com>
2022-10-24 12:45:35 +0200

    Added some functionallity to render a frame and display it in the gui and changed libvideoc from a dynamic lib to a static lib

--- project-10-20/src/main.rs
+++ project-10-20/src/main.rs
@@ -16,17 +16,27 @@ fn main() {
 }
 
 struct MyApp {
-  pub path: String,
-  pub texture: Option<egui::TextureHandle>,
-  pub scale: f32,
+  pub img_path: String,
+  pub img_texture: Option<egui::TextureHandle>,
+  pub img_scale: f32,
+
+  pub video_path: String,
+  pub video_texture: Option<egui::TextureHandle>,
+  pub video_scale: f32,
+  pub video_skip_frames: i32,
 }
 
 impl Default for MyApp {
   fn default() -> Self {
     Self {
-      path: "logo.png".to_owned(),
-      texture: None,
-      scale: 0.5
+      img_path: "logo.png".to_owned(),
+      img_texture: None,
+      img_scale: 0.5,
+
+      video_path: "".to_owned(),
+      video_texture: None,
+      video_scale: 0.2,
+      video_skip_frames: 0
     }
   }
 }
@@ -34,28 +44,82 @@ impl Default for MyApp {
 impl eframe::App for MyApp {
   fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
     egui::CentralPanel::default().show(ctx, |ui| {
-      ui.heading("Shows an image");
-      ui.add(egui::Slider::new(&mut self.scale, 0.1..=1.0).text("scale"));
-
-      if self.texture.is_some(){
-        let tex = self.texture.as_ref().unwrap();
-        ui.image(tex, tex.size_vec2()*self.scale);
-      }
-      ui.label("Pfad:");
-      ui.text_edit_singleline(&mut self.path);
-      if ui.button("Open").clicked(){
-        let path = path::Path::new(self.path.as_str());
-        if let Ok(img) = load_image_from_path(path) {
-          let tex = ui.ctx().load_texture(
-            self.path.clone(),
-            img,
-            egui::TextureFilter::Linear);
-          let _tex = self.texture.insert(tex);
-        }
-      }
-
+      self.ui_image(ctx, ui);
+      ui.separator();
+      self.ui_video_fram(ctx, ui);
     });
   }
+
+}
+impl MyApp {
+  fn ui_image(&mut self, _ctx: &egui::Context, ui: &mut egui::Ui) {
+    ui.heading("Shows an image");
+    ui.add(egui::Slider::new(&mut self.img_scale, 0.1..=1.0).text("scale"));
+
+    if self.img_texture.is_some(){
+      let tex = self.img_texture.as_ref().unwrap();
+      ui.image(tex, tex.size_vec2()*self.img_scale);
+    }
+    ui.label("Pfad:");
+    ui.text_edit_singleline(&mut self.img_path);
+    if ui.button("Open").clicked(){
+      let path = path::Path::new(self.img_path.as_str());
+      if let Ok(img) = load_image_from_path(path) {
+        let tex = ui.ctx().load_texture(
+          self.img_path.clone(),
+          img,
+          egui::TextureFilter::Linear);
+        let _tex = self.img_texture.insert(tex);
+      }
+    }
+
+  }
+  
+  fn ui_video_fram(&mut self, _ctx: &egui::Context, ui: &mut egui::Ui){
+    ui.heading("Renders a frame from a video file");
+    ui.add(egui::Slider::new(&mut self.video_scale, 0.1..=1.0).text("scale"));
+
+    if self.video_texture.is_some(){
+      let tex = self.video_texture.as_ref().unwrap();
+      ui.image(tex, tex.size_vec2()*self.video_scale);
+    }
+    ui.label("Pfad:");
+    ui.text_edit_singleline(&mut self.video_path);
+    if ui.button("Open").clicked(){
+      // let path = path::Path::new(self.video_path.as_str());
+      if let Ok(img) = load_frame_from_path(self.video_path.as_str(), self.video_skip_frames) {
+        let tex = ui.ctx().load_texture(
+          self.video_path.clone(),
+          img,
+          egui::TextureFilter::Linear);
+        let _tex = self.video_texture.insert(tex);
+      }
+    }
+  }
+
+}
+
+fn load_frame_from_path(path: &str, skip_frames: i32) -> Result<egui::ColorImage, image::ImageError>{
+  let frm;
+  match video::Frame::from(path, skip_frames) {
+    Some(_frm) => frm = _frm,
+    None => return Err(image::ImageError::IoError(std::io::Error::new(
+      std::io::ErrorKind::Other, 
+      format!("Couldn't open frame {} from {}", skip_frames, path)
+    )))
+  }
+  let (y,u,v) = (frm.channel(0), frm.channel(1), frm.channel(2));
+  let size = [frm.width() as _, frm.height() as _];
+  // egui::ColorImage::from
+  let img_buf = image::ImageBuffer::from_fn(size[0] as _, size[1] as _, |a,b| {
+    let i: usize = (a as usize)+size[0]*(b as usize);
+    let (r,g,b) = (y[i],y[i],y[i]);
+    image::Rgba([r,g,b,0 as _])
+  });
+  Ok(egui::ColorImage::from_rgba_unmultiplied(
+    size,
+    img_buf.as_flat_samples().as_slice(),
+  ))
 }
 
 fn load_image_from_path(path: &std::path::Path) -> Result<egui::ColorImage, image::ImageError> {
--- project-10-20/src/video/build.rs
+++ project-10-20/src/video/build.rs
@@ -1,9 +1,9 @@
+extern crate cmake;
+extern crate pkg_config;
 extern crate bindgen;
 
 use std::env;
 use std::path::PathBuf;
-
-use cmake;
 
 fn invoke_cmake(){
   // Builds the project in the directory located in `libfoo`, installing it
@@ -11,13 +11,15 @@ fn invoke_cmake(){
   let dst = cmake::build("libvideoc");
 
   println!("cargo:rustc-link-search=native={}", dst.display());
-  println!("cargo:rustc-link-lib=dylib=videoc");
+  println!("cargo:rustc-link-lib=static=videoc");
 }
 
 fn invoke_buildgen(){
   // Write the bindings to the $OUT_DIR/bindings.rs file.
-  let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+  let out_path = env::var("OUT_DIR").unwrap();
+  let cmake_install_datarootdir = format!("{out_path}/share"); //TODO: get this from cmake crate
 
+  /*
   // Tell cargo to look for shared libraries in the specified directory
   // println!("cargo:rustc-link-search=/path/to/lib");
   println!("cargo:rustc-link-search=libvideoc/install/libs");
@@ -25,14 +27,22 @@ fn invoke_buildgen(){
   // Tell cargo to tell rustc to link the system bzip2
   // shared library.
   println!("cargo:rustc-link-lib=videoc");
+  */
 
   // Tell cargo to invalidate the built crate whenever the wrapper changes
   println!("cargo:rerun-if-changed=wrapper_videoc.h");
+
+  std::env::set_var("PKG_CONFIG_PATH", format!("{}/pkgconfig:$PKG_CONFIG_PATH", cmake_install_datarootdir));
+  // eprintln!("{}", std::env::var("PKG_CONFIG_PATH").unwrap());
+  let _pkgconf = pkg_config::Config::new()
+    .probe("videoc")
+    .expect("Unable to config from videoc.pc");
 
   // The bindgen::Builder is the main entry point
   // to bindgen, and lets you build up options for
   // the resulting bindings.
   let bindings = bindgen::Builder::default()
+    // .clang_args(iter)
     // The input header we would like to generate
     // bindings for.
     .header("wrapper_videoc.h")
@@ -52,7 +62,7 @@ fn invoke_buildgen(){
     .expect("Unable to generate bindings");
 
   bindings
-    .write_to_file(out_path.join("bindings.rs"))
+    .write_to_file(PathBuf::from(out_path).join("bindings.rs"))
     .expect("Couldn't write bindings!");
 }
 
