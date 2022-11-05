f06ab6756f1008536f75594604f65e693b706b94
nextdorf <nextdorf@users.noreply.github.com>
2022-11-05 14:23:20 +0100

    part 3

--- project-11-04/src/lib.rs
+++ project-11-04/src/lib.rs
@@ -7,6 +7,8 @@ pub struct WinState {
   pub queue: wgpu::Queue,
   pub config: wgpu::SurfaceConfiguration,
   pub size: winit::dpi::PhysicalSize<u32>,
+
+  pub render_pipeline: wgpu::RenderPipeline,
 }
 
 impl WinState {
@@ -42,7 +44,60 @@ impl WinState {
     };
     surface.configure(&device, &config);
 
-    Self { surface, device, queue, config, size }
+    let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+      label: Some("Shader"),
+      source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
+    });
+
+    let render_pipeline_layout = device.create_pipeline_layout(
+      &wgpu::PipelineLayoutDescriptor {
+        label: Some("Render Pipeline Layout"),
+        bind_group_layouts: &[],
+        push_constant_ranges: &[],
+    });
+
+    let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+      label: Some("Render Pipeline"),
+      layout: Some(&render_pipeline_layout),
+      vertex: wgpu::VertexState {
+        module: &shader,
+        entry_point: "vs_main", // 1.
+        buffers: &[], // 2.
+      },
+      fragment: Some(wgpu::FragmentState { // 3.
+        module: &shader,
+        entry_point: "fs_main",
+        targets: &[Some(wgpu::ColorTargetState { // 4.
+          format: config.format,
+          blend: Some(wgpu::BlendState::REPLACE),
+          write_mask: wgpu::ColorWrites::ALL,
+        })],
+      }),
+
+      primitive: wgpu::PrimitiveState {
+        topology: wgpu::PrimitiveTopology::TriangleList, // 1.
+        strip_index_format: None,
+        front_face: wgpu::FrontFace::Ccw, // 2.
+        cull_mode: Some(wgpu::Face::Back),
+        // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
+        polygon_mode: wgpu::PolygonMode::Fill,
+        // Requires Features::DEPTH_CLIP_CONTROL
+        unclipped_depth: false,
+        // Requires Features::CONSERVATIVE_RASTERIZATION
+        conservative: false,
+      },
+
+      depth_stencil: None, // 1.
+      multisample: wgpu::MultisampleState {
+        count: 1, // 2.
+        mask: !0, // 3.
+        alpha_to_coverage_enabled: false, // 4.
+      },
+      multiview: None, // 5.
+    });
+ 
+
+    Self { surface, device, queue, config, size, render_pipeline }
   }
   pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
     if new_size.width > 0 && new_size.height > 0 {
@@ -65,26 +120,29 @@ impl WinState {
         label: Some("Some encoder"),
       }
     );
-    {
-      let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-      // encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-        label: Some("Render Pass"),
-        color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-          view: &view,
-          resolve_target: None,
-          ops: wgpu::Operations {
-            load: wgpu::LoadOp::Clear(wgpu::Color {
-              r: 0.9,
-              g: 0.0,
-              b: 0.3,
-              a: 1.0,
-            }),
-            store: true,
-          },
-        })],
-        depth_stencil_attachment: None,
-      });
-    }
+    let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+    // encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+      label: Some("Render Pass"),
+      color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+        view: &view,
+        resolve_target: None,
+        ops: wgpu::Operations {
+          load: wgpu::LoadOp::Clear(wgpu::Color {
+            r: 0.9,
+            g: 0.0,
+            b: 0.3,
+            a: 1.0,
+          }),
+          store: true,
+        },
+      })],
+      depth_stencil_attachment: None,
+    });
+    render_pass.set_pipeline(&self.render_pipeline);
+    render_pass.draw(0..3, 0..1);
+    drop(render_pass);
+
+
     self.queue.submit(std::iter::once(encoder.finish()));
     output.present();
     Ok(())
